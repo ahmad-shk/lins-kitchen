0ad5d8fa619fe80ce03046c5560a95459c32d629
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2025-12-20 23:30:43 +0500

    update

--- app/lunch-menus/page.tsx
+++ app/lunch-menus/page.tsx
@@ -1,19 +1,100 @@
-import FeatureItem from "@/components/feature-item"
-import InnerBanner from "@/components/inner-banner"
-import PageInfo from "@/components/page-info"
-import ProductsListing from "@/components/products-listing"
+"use client"
+import React from 'react'
+import InnerBanner from '@/components/inner-banner'
+import PageInfo from '@/components/page-info'
 
-export default function LunchMenu() {
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+const Menu = () => {
   return (
-    <div className="min-h-screen">
-      <InnerBanner image="/images/lunch-menu-banner.png" title="UNSERE MITTAGSMENÜS" />
-      <PageInfo 
-        badge="Mittagsmenü" 
-        title="Frisch zubereitet. Perfekt für Ihre Mittagspause."
-        description="Sorgfältig zusammengestellte Mittagsgerichte, zubereitet mit erstklassigen Zutaten und raffinierten Aromen. Perfekt für eine entspannte und zugleich luxuriöse Auszeit am Mittag."
-      />
-      <FeatureItem/>
-      <ProductsListing />
-    </div>
+    <main>
+        <InnerBanner image="/images/menu-banner.png" title="Speisekarte" />
+        <PageInfo
+            badge="Speisekarte" 
+            title="Asiatische Küche mit Sorgfalt zubereitet"
+            description="Genießen Sie frische asiatische Küche mit Wok-Gemüse, knuspriger Ente, duftenden Currys und Sushi. Jedes Gericht wird mit aromatischen Gewürzen und leichten Saucen sorgfältig zubereitet; es gibt eine große Auswahl an vegetarischen Optionen."
+        />
+        <div className='bg-navyBlue'>
+            <div className='max-w-[1176px] px-4 mx-auto'>
+                <Swiper
+                modules={[Navigation]}
+                navigation
+                loop
+                centeredSlides={true}
+                slidesPerView={1.4}
+                spaceBetween={0}
+                className="menu-swiper py-20!"
+                breakpoints={{
+                    767: {
+                        slidesPerView: 1.9,
+                    },
+                }}
+                >
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-1.jpg"
+                        alt="Menu 1"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-2.jpg"
+                        alt="Menu 2"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-3.jpg"
+                        alt="Menu 3"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-4.jpg"
+                        alt="Menu 4"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-5.jpg"
+                        alt="Menu 5"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-6.jpg"
+                        alt="Menu 6"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Menu-7.jpg"
+                        alt="Menu 7"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                        src="/images/Mittagsmenu.jpg"
+                        alt="Mittagsmenu"
+                        className="mx-auto"
+                        />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    </main>
   )
 }
+
+export default Menu
