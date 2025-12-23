c0bb3f35ac21f72cd952b7ba627b7bef33a0e4b5
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2025-12-24 04:33:10 +0500

    u[date

--- components/products-listing.tsx
+++ components/products-listing.tsx
@@ -150,10 +150,17 @@ const ProductsListing = () => {
               <Image src="/images/image-15.jpg" width={380} height={540} alt="product"/>
             </div>
           </div>
-           <div className="bg-navyBlue rounded-2xl p-4 sm:p-5 gap-3 sm:gap-5 text-white flex items-center justify-between">
-            <span className="bg-btnSecondary rounded-md inline-block text-navyBlue text-lg sm:text-2xl tracking-tighter py-1.5 sm:py-2.5 px-1.5" >M28</span>
-            <h4 className="text-lg sm:text-2xl uppercase flex-1 max-sm:leading-snug">4 Lachssushi + 6 Californiamaki</h4>
-            <span className="text-btnSecondary text-2xl lg:text-[30px] tracking-tighter">€ 13,90</span>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="bg-navyBlue rounded-2xl p-4 sm:p-5 gap-3 sm:gap-5 text-white flex items-center justify-between">
+              <span className="bg-btnSecondary rounded-md inline-block text-navyBlue text-lg sm:text-2xl tracking-tighter py-1.5 sm:py-2.5 px-1.5" >M28</span>
+              <h4 className="text-lg sm:text-2xl uppercase flex-1 max-sm:leading-snug">4 Lachssushi + 6 <br className="max-sm:hidden"/> Californiamaki</h4>
+              <span className="text-btnSecondary text-2xl lg:text-[30px] tracking-tighter">€ 13,90</span>
+            </div>
+            <div className="bg-navyBlue rounded-2xl p-4 sm:p-5 gap-3 sm:gap-5 text-white flex items-center justify-between">
+              <span className="bg-btnSecondary rounded-md inline-block text-navyBlue text-lg sm:text-2xl tracking-tighter py-1.5 sm:py-2.5 px-1.5" >M23</span>
+              <h4 className="text-lg sm:text-2xl uppercase flex-1 max-sm:leading-snug">Acht-Schätze</h4>
+              <span className="text-btnSecondary text-2xl md:text-[30px] tracking-tighter">€ 11,90</span>
+            </div>
           </div>
         </div>
 
@@ -165,32 +172,32 @@ const ProductsListing = () => {
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M15</span>
                   <span className="flex-1 font-normal">Gebackener Seelachs süß-sauer  A,M,D</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">10,90</span>
                 </li>
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M16</span>
                   <span className="flex-1 font-normal">Gebackener Seelachs mit Wokgemüse A,F,D</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">11,50</span>
                 </li>
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M17</span>
                   <span className="flex-1 font-normal">Tofu nach Szechuan-Art  A,G</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">9,90</span>
                 </li>
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M18</span>
                   <span className="flex-1 font-normal">Gebratenes Wokgemüse</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">9,50</span>
                 </li>
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M19</span>
                   <span className="flex-1 font-normal">Garnelen mit Wokgemüse A,F,B</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">13,20</span>
                 </li>
                 <li className="flex items-center justify-between gap-2.5">
                   <span className="font-normal inline-block min-w-9">M20</span>
                   <span className="flex-1 font-normal">Lachs Teriyaki mit Gemüse N,F,D</span>
-                  <span className="font-medium">€ 9,50</span>
+                  <span className="font-medium">13,20</span>
                 </li>
               </ul>
             </div>
@@ -199,9 +206,9 @@ const ProductsListing = () => {
             </div>
           </div>
           <div className="bg-navyBlue rounded-2xl p-4 sm:p-5 gap-3 sm:gap-5 text-white flex items-center justify-between">
-            <span className="bg-btnSecondary rounded-md inline-block text-navyBlue text-lg sm:text-2xl tracking-tighter py-1.5 sm:py-2.5 px-1.5" >M23</span>
-            <h4 className="text-lg sm:text-2xl uppercase flex-1 max-sm:leading-snug">6 Lachs-Sushi + 6 Lachs-<br className="max-sm:hidden"/> Lemuren</h4>
-            <span className="text-btnSecondary text-2xl md:text-[30px] tracking-tighter">€ 13,90</span>
+            <span className="bg-btnSecondary rounded-md inline-block text-navyBlue text-lg sm:text-2xl tracking-tighter py-1.5 sm:py-2.5 px-1.5" >M29</span>
+            <h4 className="text-lg sm:text-2xl uppercase flex-1 max-sm:leading-snug">General Zhus <br className="max-sm:hidden"/>Huhn</h4>
+            <span className="text-btnSecondary text-2xl md:text-[30px] tracking-tighter">€ 11,90</span>
           </div>
         </div>
 
